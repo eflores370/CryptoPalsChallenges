82be7090e29d2053a2c0fcd7bf7f18d7259df543
Emanuel Flores <emanuel.flores@nccgroup.trust>
2019-04-09 16:05:03 -0700

    updated challenge 4

--- s1c4.go
+++ s1c4.go
@@ -1,8 +1,7 @@
 package main
 
 import "fmt"
-// import "encoding/hex"
-// import "io/ioutil"
+import "encoding/hex"
 import "bufio"
 import "os"
 
@@ -21,6 +20,14 @@ func readLines(path string) ([]string) {
 	return lines
 }
 
+// func score() {
+// englishFreq := map[string]float32{
+// 	"E": 12.70, "T": 9.06, "A": 8.17, "O": 7.51, "I": 6.97,
+// 	"N": 6.75, "S": 6.33, "H": 6.09, "R": 5.99, "D": 4.25, "L": 4.03, 
+// 	"C": 2.78, "U": 2.76, "M": 2.41, "W": 2.36, "F": 2.23, "G": 2.02, 
+// 	"Y": 1.97, "P": 1.93, "B": 1.29, "V": 0.98, "K": 0.77, "J": 0.15, 
+//  	"X": 0.15, "Q": 0.10, "Z": 0.07}
+// }
 
 
 func main() {
@@ -28,10 +35,12 @@ func main() {
 	// data,_ := ioutil.ReadFile("4.txt")
 	// fmt.Print(string(data))
 
-	lines := readLines("4.txt")
+	lines := readLines("files/4.txt")
 
 	for i := range lines {
-		fmt.Println(lines[i])
+		// fmt.Println(lines[i])
+		decoded,_ := hex.DecodeString(lines[i])
+		fmt.Println(string(decoded))
 	}
 	
 }
