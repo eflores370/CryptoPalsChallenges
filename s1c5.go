6831e841ce987c8faa427994c53df34d8dfda61b
Emanuel Flores <emanuel.flores@nccgroup.trust>
2019-04-10 18:03:54 -0700

    End of Challenge 5

--- s1c5.go
+++ s1c5.go
@@ -5,26 +5,16 @@ import "encoding/hex"
 
 func main(){
 
-	s1 := "Burning 'em, if you ain't quick and nimble"
-	s2 := "\nI go crazy when I hear a cymbal"
+	s := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 
-	rawBytes1 := []byte(s1)
-	rawBytes2 := []byte(s2)
+	rawBytes1 := []byte(s)
 
 	k := []byte("ICE")
-
-	fmt.Println(hex.EncodeToString(rawBytes1))
-	fmt.Println(hex.EncodeToString(rawBytes2))
 
 	for i := range rawBytes1 {
 		rawBytes1[i] ^= k[i%3]
 	}
 
-	for i := range rawBytes2 {
-		rawBytes2[i] ^= k[i%3]
-	}
-
 	fmt.Println(hex.EncodeToString(rawBytes1))
-	fmt.Println(hex.EncodeToString(rawBytes2))
 
 }
