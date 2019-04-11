41585e71e2fea08e397b04cd0fe2f46abe941a3c
Emanuel Flores <emanuel.flores@nccgroup.trust>
2019-04-11 15:54:57 -0700

    Removing retrieveUmask

--- s1c6.go
+++ s1c6.go
@@ -3,17 +3,20 @@ package main
 import "os"
 import "fmt"
 import "bufio"
-import "encoding/base64"
+// import "encoding/base64"
 
 // Returns the Hamming distance of two strings
 // Input: Binary String 1 (String), Binary String 2 (String)
 // Output: Distance (int)
 
-func hammingDist(string1, string2 string) (distance int) {
+func hammingDist(block1, block2 []byte) (distance int) {
+	
+	blockA := ToBin(block1)
+	blockB := ToBin(block2)
 
-	for i := range string1 {
-		if (string1[i] != string2[i]) {
-			distance ++
+	for i := range blockA {
+		if blockA[i] != blockB[i] {
+			distance++
 		}
 	}
 
@@ -22,14 +25,34 @@ func hammingDist(string1, string2 string) (distance int) {
 }
 
 
-// Convert a text string to a binary string
-// Input: Text (String)
+// TO Remove
+// func retrieveUMask(byteArr []byte) (umask string) {
+
+// 	for i := range byteArr {
+// 		for j := 2; j < 255; j *= 2 {
+// 			fmt.Println(byteArr[i],j)
+// 			fmt.Println(byteArr[i] & byte(j) != 0)
+// 			if (byteArr[i] & byte(j) != 0){
+// 				umask += "0"	
+// 			} else {
+// 				umask += "1"
+// 			}
+// 		}
+// 	}
+// 	fmt.Println(umask)
+
+// 	return umask
+// }
+
+
+// Convert a byte array to a binary string
+// Input: Byte Array ([]Byte)
 // Output: Binary (String)
 
-func stringToBin(string1 string) (binary string) {
+func ToBin(byteArr []byte) (binary string) {
 
-	for i := range string1 {
-		binary += fmt.Sprintf("%08b", byte(string1[i]))
+	for i := range byteArr {
+		binary += fmt.Sprintf("%08b", byteArr[i])
 	}
 
 	return binary
@@ -42,18 +65,18 @@ func recoverloop() {
 	}
 }
 
-func findKEYSIZE(s string) {
+// func findKEYSIZE(s string) {
 	
 
-	for KEYSIZE := 2; KEYSIZE < 40; KEYSIZE++ {
-		defer recoverloop()
-		fmt.Println(stringToBin(s[:KEYSIZE]))
-		fmt.Println(stringToBin(s[KEYSIZE:KEYSIZE*2]))
-		fmt.Println(hammingDist(stringToBin(s[:KEYSIZE]),stringToBin(s[KEYSIZE:KEYSIZE*2]))/KEYSIZE)
+// 	for KEYSIZE := 2; KEYSIZE < 40; KEYSIZE++ {
+// 		defer recoverloop()
+// 		fmt.Println(stringToBin(s[:KEYSIZE]))
+// 		fmt.Println(stringToBin(s[KEYSIZE:KEYSIZE*2]))
+// 		fmt.Println(hammingDist(stringToBin(s[:KEYSIZE]),stringToBin(s[KEYSIZE:KEYSIZE*2]))/KEYSIZE)
 
-	}
+// 	}
 
-}
+// }
 
 func readFile(path string) ([]string) {
 	file, _ := os.Open(path)
@@ -71,8 +94,8 @@ func readFile(path string) ([]string) {
 
 func main() {
 
-	s := "this is a test"
-	s1 := "wokka wokka!!!"
+	s := []byte("this is a test")
+	s1 := []byte("wokka wokka!!!")
 
 	// file := readFile("files/6.txt")
 
@@ -90,7 +113,17 @@ func main() {
 
 	// a, _ := base64.StdEncoding.DecodeString(lines)
 
-	findKEYSIZE(s)
+	// findKEYSIZE(s)
 
-	fmt.Println("HELLO")
+	// fmt.Println("HELLO")
+
+	// hammingDist(byte)
+
+	fmt.Println(s)	
+
+	// fmt.Println(hammingDist(s,s1))
+
+	fmt.Println(ToBin(s))
+
+	fmt.Println(hammingDist(s,s1))
 }
