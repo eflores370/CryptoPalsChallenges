de6415f6b2f4f50f39be7a726948f710688222f5
Emanuel Flores <emanuel.flores@nccgroup.trust>
2019-04-11 13:18:24 -0700

    readFiles

--- s1c6.go
+++ s1c6.go
@@ -1,6 +1,9 @@
 package main
 
+import "os"
 import "fmt"
+import "bufio"
+import "encoding/base64"
 
 // Returns the Hamming distance of two strings
 // Input: Binary String 1 (String), Binary String 2 (String)
@@ -44,7 +47,6 @@ func findKEYSIZE(s string) {
 
 	for KEYSIZE := 2; KEYSIZE < 40; KEYSIZE++ {
 		defer recoverloop()
-		fmt.Println("\n\n",KEYSIZE)
 		fmt.Println(stringToBin(s[:KEYSIZE]))
 		fmt.Println(stringToBin(s[KEYSIZE:KEYSIZE*2]))
 		fmt.Println(hammingDist(stringToBin(s[:KEYSIZE]),stringToBin(s[KEYSIZE:KEYSIZE*2]))/KEYSIZE)
@@ -53,10 +55,36 @@ func findKEYSIZE(s string) {
 
 }
 
+func readFile(path string) ([]string) {
+	file, _ := os.Open(path)
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
+
 func main() {
 
 	s := "this is a test"
 	// s1 := "wokka wokka!!!"
+
+	file := readFile("files/6.txt")
+
+	var lines string
+
+	for i := range file {
+		lines += file[i]
+	}
+
+	fmt.Println(lines)
+	fmt.Println(base64.StdEncoding.DecodeString(lines))
+	
 
 	// fmt.Println(hammingDist(stringToBin(s),stringToBin(s1)))
 
