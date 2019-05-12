0b3aa06bc5a611b2933a8622407096673de3c482
Emanuel F <emanuelcflores@gmail.com>
2019-05-11 21:29:35 -0700

    Replaced s1c6
    Signed-off-by: Emanuel Flores <emanuel.flores@nccgroup.trust>

--- s1c6.go
+++ s1c6.go
@@ -1,74 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
 	"os"
-	//"sort"
+	"sort"
 	"strings"
 )
-import "fmt"
-import "bufio"
-import "encoding/base64"
 
 type scores struct {
 	scoreResult float32
 	rawBytes    []byte
+	key         int
 }
 
-// Returns the Hamming distance of two strings
-// Input: Byte Array1 ([]byte), Byte Array2 ([]byte)
-// Output: Distance (int)
-
-func hammingDist(block1, block2 []byte) (distance int) {
-
-	binBlock1 := ToBin(block1)
-	binBlock2 := ToBin(block2)
-
-	for i := range binBlock1 {
-		if binBlock1[i] != binBlock2[i] {
-			distance++
-		}
-	}
-
-	return distance
-
-}
-
-// Convert a byte array to a binary string
-// Input: Byte Array ([]Byte)
-// Output: Binary (String)
-
-func ToBin(byteArr []byte) (binary string) {
-
-	for i := range byteArr {
-		binary += fmt.Sprintf("%08b", byteArr[i])
-	}
-
-	return binary
-
-}
-
-// Recovers a loop from Runtime exceptions
-
-func recoverloop() {
-	if r := recover(); r != nil {
-		fmt.Println("Recovered from ", r)
-	}
-}
-
-// Determine the probable hamming distance for a given byte arry
-// Input: Byte Array ([]byte)
-
-func findKeysize(byteArr []byte) {
-
-	for KEYSIZE := 2; KEYSIZE <= 40; KEYSIZE++ {
-		defer recoverloop()
-		fmt.Println("Keysize:", KEYSIZE)
-		// fmt.Print(byteArr[:KEYSIZE])
-		// fmt.Println(byteArr[KEYSIZE:KEYSIZE*2])
-		fmt.Println("Hamming Distance:", (hammingDist(byteArr[:KEYSIZE], byteArr[KEYSIZE:KEYSIZE*2]))/KEYSIZE)
-	}
-}
-
+// ReadFile
 // Parse a file and returns the contents
 // Input: File Path (String)
 // Output: Array of Strings ([]string)
@@ -86,6 +33,73 @@ func readFile(path string) []string {
 	return lines
 }
 
+// ToBin
+// Convert a byte array to a binary string
+// Input: Byte Array ([]Byte)
+// Output: Binary (String)
+
+func ToBin(byteArr []byte) (binary string) {
+
+	for i := range byteArr {
+		binary += fmt.Sprintf("%08b", byteArr[i])
+	}
+
+	return binary
+
+}
+
+// HammingDist
+// Returns the Hamming distance of two strings
+// Input: Byte Array1 ([]byte), Byte Array2 ([]byte)
+// Output: Distance (int)
+func hammingDist(block1, block2 []byte) (distance float32) {
+
+	binBlock1 := ToBin(block1)
+	binBlock2 := ToBin(block2)
+
+	for i := range binBlock1 {
+		if binBlock1[i] != binBlock2[i] {
+			distance++
+		}
+	}
+
+	return distance
+
+}
+
+// FindKeySize
+func findKeySize(initial, final int, byteArr []byte) (keysize int) {
+
+	var shortestDist float32 = 255
+
+	for KEYSIZE := initial; KEYSIZE <= final; KEYSIZE++ {
+		//defer recoverloop()
+
+		tmpArr := breakCipherBlocks(byteArr, KEYSIZE)
+		var total float32 = 0
+
+		distances := make([]float32, 0)
+		for i := range tmpArr {
+			if i+2 < len(tmpArr) {
+				distances = append(distances, hammingDist(tmpArr[i], tmpArr[i+1])/float32(KEYSIZE))
+			}
+		}
+
+		for i := range distances {
+			total += distances[i]
+		}
+
+		currentDist := float32(total / float32(len(distances)))
+
+		if currentDist < shortestDist {
+			shortestDist = currentDist
+			keysize = KEYSIZE
+		}
+	}
+
+	return keysize
+}
+
 // Given a large array, split the array into smaller array with the maximum size of Keysize
 // Input: Large byte arr ([]byte) & Max length of array (int)
 // Output: A Large array containing smaller arrays ([][]byte)
@@ -99,7 +113,7 @@ func breakCipherBlocks(byteArr []byte, Keysize int) (cipherBlocks [][]byte) {
 
 			if i+j < len(byteArr) {
 				smallArry = append(smallArry, byteArr[i+j])
-				defer recoverloop()
+				//defer recoverloop()
 			}
 		}
 		cipherBlocks = append(cipherBlocks, smallArry)
@@ -123,26 +137,19 @@ func transposeBlocks(Arr [][]byte, Keysize int) (Arr2 [][]byte) {
 	return Arr2
 }
 
-// Brute force the XOR Key for a given Byte array and return a an array of scores
-// Input: Byte array ([]byte)
-// Output: Array of byte arrays
-func bruteForce(ByteArr []byte) (XORByteArr [][]byte) {
-
-	// Brute force every character
-	for i := 0; i < 255; i++ {
-		tmpByteArr := make([]byte, len(ByteArr))
-		copy(tmpByteArr, ByteArr)
-		result := XOR(tmpByteArr, byte(i))
-		XORByteArr = append(XORByteArr, result)
-
+// XOR a raw byte array with a given key
+// Input: Byte Array ([]byte) & Key ([]byte)
+func XOR(rawBytes []byte, key byte) []byte {
+	for i := range rawBytes {
+		rawBytes[i] ^= key
 	}
-	return XORByteArr
+	return rawBytes
 }
 
 // Scoring function to determine if output is probable English
 // Input: Array of bytes ([]Byte)
 // Output: A score struct containing total points and a byte array ({int, []byte})
-func score(rawBytes []byte) scores {
+func score(rawBytes []byte, value int) scores {
 	englishFreq := map[string]float32{
 		"E": 12.70, "T": 9.06, "A": 8.17, "O": 7.51, "I": 6.97,
 		"N": 6.75, "S": 6.33, "H": 6.09, "R": 5.99, "D": 4.25, "L": 4.03,
@@ -161,35 +168,41 @@ func score(rawBytes []byte) scores {
 		}
 	}
 
-	//fmt.Println(string(rawBytes), totalScore)
+	//fmt.Println(rawBytes, totalScore, value)
 
-	return scores{scoreResult: totalScore, rawBytes: rawBytes}
+	return scores{scoreResult: totalScore, rawBytes: rawBytes, key: value}
 }
 
-// XOR a raw byte array with a given key
-// Input: Byte Array ([]byte) & Key ([]byte)
-func XOR(rawBytes []byte, key byte) []byte {
-	for i := range rawBytes {
-		rawBytes[i] ^= key
+// Brute force the XOR Key for a given Byte array and return a an array of scores
+// Input: Byte array ([]byte)
+// Output: Array of byte arrays
+func bruteForce(ByteArr []byte) (XORByteArr []scores) {
+
+	// Brute force every character
+	for i := 0; i < 255; i++ {
+		tmpByteArr := make([]byte, len(ByteArr))
+		copy(tmpByteArr, ByteArr)
+		result := XOR(tmpByteArr, byte(i))
+		XORByteArr = append(XORByteArr, score(result, i))
+
 	}
-	return rawBytes
+	return XORByteArr
+}
+
+// Recovers a loop from Runtime exceptions
+
+func recoverloop() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovered from ", r)
+	}
 }
 
 func rebuildString(finalArry []scores, Keysize int) (finalString []byte) {
-
-	//var Output string;
-
-	//for i := range finalArry {
-	//	fmt.Println(finalArry[i])
-	//}
-
-	//fmt.Println(i,rawBytes)
 
 	for j := 0; j < Keysize; j++ {
 		for i := 0; i < len(finalArry[0].rawBytes); i++ {
 			for _, v := range finalArry {
 				rawBytes := v.rawBytes
-				//fmt.Print(rawBytes[j])
 				finalString = append(finalString, rawBytes[i])
 				defer recoverloop()
 			}
@@ -199,35 +212,38 @@ func rebuildString(finalArry []scores, Keysize int) (finalString []byte) {
 	return finalString
 }
 
-// Vigenère cipher brute force
-
 func main() {
 
 	var lines string
-	modifiedArray := make([][]byte, 0)
-	tmpArr := make([][]byte, 5)
+	//scoreList := make([][]byte, 0)
+	finalArray := make([]scores, 0)
 
-	// Place lines from file into large byte array
+	// Read file and base 64 decode
 	file := readFile("files/6.txt")
 	for i := range file {
 		lines += file[i]
 	}
 	decoded, _ := base64.StdEncoding.DecodeString(lines)
 
-	// Keysize is probably 5
-	//findKeysize(decoded)
+	keysize := findKeySize(2, 42, decoded)
 
-	chunckedArry := breakCipherBlocks(decoded, 5)
-	transposedArray := transposeBlocks(chunckedArry, 5)
+	splitArray := breakCipherBlocks(decoded, keysize)
+	ModifiedArray := transposeBlocks(splitArray, keysize)
 
-	for i := range transposedArray {
-		tmpArr[i] = bruteForce(transposedArray[i])
-		modifiedArray = append(modifiedArray, tmpArr[i])
-
+	for i := range ModifiedArray {
+		tmpArray := bruteForce(ModifiedArray[i])
+		sort.Slice(tmpArray, func(a, b int) bool { return tmpArray[a].scoreResult < tmpArray[b].scoreResult })
+		finalArray = append(finalArray, tmpArray[len(tmpArray)-1])
 	}
 
-	fmt.Println(transposedArray)
+	//fmt.Println(rebuildString(finalArray, keysize))
+	fmt.Println(string(rebuildString(finalArray, keysize)))
 
-	//fmt.Println(rebuildString(finalArry, 5))
-	//fmt.Println(string(rebuildString(finalArry, 5)))
+	// Key
+	fmt.Print("Key: ")
+	for i := range finalArray {
+		fmt.Print(string(finalArray[i].key))
+	}
+	//rebuildString(finalArray, keysize)
+
 }
